167b183fbb4bdcd4a781c8cfd0555f2d65849819
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-22 01:27:41 -0500

    add tech stack js sphere

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -16,3 +16,38 @@ document.addEventListener('DOMContentLoaded', function() {
 		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
 	});
 }, false);
+// tech stack sphere logic using tagCloud
+// tech stack array
+const myTags = [
+    'HTML', 'CSS', 'JavaScript',
+    'jQuery', 'Bulma CSS', 'Bootstrap',
+    'Node.js', 'Express.js', 'npm',
+    'Jest', 'GitHub', 'Heroku', 'git', 'Adobe Illustrator',
+     'MySQL', 'Sequelize',
+     'Adobe Photoshop'
+];
+// declare tagcloud function for id techSpere
+// with tech stack array
+var tagCloud = TagCloud('#techSphere', myTags,{
+
+  // radius in px
+  radius: 150,
+
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'fast',
+  initSpeed: 'fast',
+
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+
+  // interact with cursor move on mouse out
+  keep: true
+
+}); 
+var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+// pick and set text to random color from colors array on reload
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('#techSphere').style.color = random_color;
